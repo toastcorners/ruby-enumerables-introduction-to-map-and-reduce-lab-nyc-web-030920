9d54a768b2332d7b088aa107f49ef128221ea531
Melissa Wise <wise.melissa19@gmail.com>
2020-03-05 03:05:19 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -52,8 +52,9 @@ end
 def reduce_to_total(source_array, starting_point = 0)
 i = 0
 almost_result = []
+sum = 0
 while i < source_array.length
-  the_work = source_array[i] += starting_point
+  sum = source_array[i] += starting_point
   almost_result << the_work
   i += 1
 end #=> i loop
